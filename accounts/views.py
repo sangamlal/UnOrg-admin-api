0eccc2a98e56aa779268b91a6ed2dedd3bba0c02
nikhilkotiya8 <nikhilkotiya8@gmail.com>
2022-11-14 20:09:27 +0530

    order_with_cord

--- accounts/views.py
+++ accounts/views.py
@@ -362,9 +362,9 @@ class SendZohoRegistrationLink_fun(APIView):
                                                         </a>
                                                         <br>
                                                         <strong>Open This URL To Enter Credentials</strong>
-                                                        http://localhost:5173/add-credential?id="""+str(zohodata.id)+""""
+                                                        https://onlinetheladev.online//add-credential?id="""+str(zohodata.id)+""""
                                                         <strong>Redirect URL</strong>
-                                                        http://localhost:5173/add-access
+                                                        https://onlinetheladev.online//add-access
                                                     </td>
                                                 </tr>
                                             </tbody>
